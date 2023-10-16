46b17cf5bd0391350d2450da18d6db7bf03d1b55
PFT\EricYC_Yang <EricYC_Yang@PerfectCorp.com>
2023-10-16 18:58:26 +0800

    feat: ironman_head v1

---
+++ src/Ironman.js
@@ -0,0 +1,12 @@
+import './Ironman.css';
+import Head from './components/Head';
+
+const Ironman = () => {
+  return (
+    <div className="ironman">
+      <Head></Head>
+    </div>
+  );
+}
+
+export default Ironman;
